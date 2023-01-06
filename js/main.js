a1c0b7bda516d15aa059b8f58eea9395cbb4b900
Irving Saenz Colon <irving.saenz.colon@gmail.com>
2023-01-06 11:08:04 -0600

    Setting up main files. html has basic layout, css contains general styles and main.js includes the basic code to get canvas and get its context

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// HTML elements
+const canvas = document.getElementById('game');
+
+
+// Eventslistener
+window.addEventListener('load', draw);
+
+// canvas context
+const context = canvas.getContext('2d');
+
+// Drawing
+
+function draw(){
+    
+}
